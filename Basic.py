8770a4923edab7035a0b3040f4fab0e3eae6f144
JFortun <josemanuelfortunromero@gmail.com>
2021-02-04 11:32:29 +0100

    Basic

---
+++ Basic.py
@@ -0,0 +1,20 @@
+#Variables
+
+name = "Antonio"
+age = 20
+color1, color2, color3 = "Red", "Green", "Blue"
+number1, number2, number3 = 5, 13, 25.5
+message = """ This is
+a message"""
+print("The cat is " + color1)
+
+#Operators
+
+print(number1 + number2)
+print(number1 - number2)
+print(number2 / number1)
+print(number2 // number1)
+print(number1 * number2)
+print(number3 % number1)
+print(type(number3))
+print(message)
